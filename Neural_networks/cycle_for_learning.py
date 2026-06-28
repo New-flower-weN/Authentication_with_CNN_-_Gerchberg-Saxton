989c8bbfe4e54ed839bbb43ff103d8e1a90eac2f
Flower <lol.no.19801@mail.ru>
2026-06-28 21:58:57 +0300

    first commit

--- Neural_networks/cycle_for_learning.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-for num_of_folders in (2**i for i in range(5)):
-    # print(num_of_folders)
-    # proc = subprocess.run(["./myenv/Scripts/activate"])
-    proc = subprocess.run(["./myenv/Scripts/python", "./Neural_networks/unet.py", f"{num_of_folders}"])
